3490859c13e61b8cf81ef56d9c731cf58868e327
Mike Xu <feifeidadi@163.com>
2026-04-23 14:51:13 -0400

    Improve the OSI symbol parser by parsing it from back to front.

--- osiSymbolParser.C
+++ osiSymbolParser.C
@@ -1,90 +1,89 @@
 /* 
  * OSI Symbol format:
- * ROOT + YYMMDD + C/P + STRIKE (scaled)
+ * Symbol + YYMMDD + C/P + STRIKE (scaled)
  *
- * ROOT 1–6 characters (6-character field that is right-padded with spaces)
+ * Symbol 1–6 characters (6-character field that is right-padded with spaces)
  * Strike is usually:
  *   8 digits
  *   implied decimal (divide by 1000)
  *
- * TSLA  250216P00500000 // Underlying stock: TSLA, Expiration: 2025-02-16, Put option,  Stike  = 50.000
+ * TSLA  250216P00500000 // Underlying stock: TSLA, Expiration: 2025-02-16, Put Option,  Stike  = 50.000
  * AAPL  240119C00180000 // Underlying stock: APPL, Expiration: 2024-01-19, Call Option, Strike = 180.000
  */
 
  // Compile: g++ -std=c++17 -Wall -Wextra -O2 osiSymbolParser.C -o osiSymbolParser
 
 #include <iostream>
-#include <charconv> // from_chars()
-
-#define OSI_SYMBOL_LENGTH 21
-#define EXPIRATION_DATE_LENGTH 6
+#include <cstdint>
+#include <charconv> // std::from_chars()
 
 struct osiSymbol
 {
   public:
-    std::string rootSymbol = "";
-    std::string expirationDate = "";
-    char callOrPut = '\0';
-    float strikePrice = 0.0;
+    std::string symbol{};
+    std::string expirationDate{};
+    char callOrPut{'\0'};   // CallOrPut is the 9th character from the end
+    float strikePrice{0.0}; // Parse the last 8 characters as an integer and divide it by 1000
 
-    void getRootSymbol(const std::string& osiSymbol_)
+    void printSymbolInfo() const
     {
-      pos = osiSymbol_.find(' '); // Find symbol
+      std::cout << "Symbol     : " << symbol << std::endl
+                << "Expiration : " << expirationDate << std::endl
+                << "CallOrPut  : " << callOrPut << std::endl
+                << "Strike     : " << strikePrice << std::endl
+                << std::endl;
+    }
+
+    void getSymbol(const std::string& osiSymbol_)
+    {
+      auto pos = osiSymbol_.find(' '); // Find symbol
       if (pos == std::string::npos)
       {
-        pos = 6; // 6 characters root symbol
+        pos = maxSymbolLength;
       }
 
-      rootSymbol = osiSymbol_.substr(0, pos);
-      if (pos < 6)
-      {
-        pos++; // Point to expiration date after getting root symbol
-      }
+      symbol = osiSymbol_.substr(0, pos);
     }
 
     void getOsiSymbolInfo(const std::string& osiSymbol_)
     { 
-      // TODO: Getting osi symbol info in the reversed sequence to avoid using pos internal variable.
-      getRootSymbol(osiSymbol_);
+      osiSymbolLen = osiSymbol_.length();
+
+      getSymbol(osiSymbol_);
       getExpirationDate(osiSymbol_);
       getCallOrPut(osiSymbol_);
       getStrikePrice(osiSymbol_);
     }
 
+  private:
     void getExpirationDate(const std::string& osiSymbol_)
     {
-      expirationDate = osiSymbol_.substr(pos, EXPIRATION_DATE_LENGTH);
-      // Strip space at beginning if it's available
-      pos += EXPIRATION_DATE_LENGTH;
+      const size_t start = osiSymbolLen - strikePriceStrLen - 1 - expirationDateLength;
+      expirationDate = osiSymbol_.substr(start, expirationDateLength);
     }
 
     void getCallOrPut(const std::string& osiSymbol_)
     {
-      callOrPut = osiSymbol_[pos++];
+      const auto pos = osiSymbolLen - strikePriceStrLen -1;
+      callOrPut = osiSymbol_[pos];
     }
 
     void getStrikePrice(const std::string& osiSymbol_)
     {
-      const auto len = osiSymbol_.length();
-      if (std::from_chars(osiSymbol_.data() + len - 8, osiSymbol_.data() + len, strikePrice).ec != std::errc{})
+      if (std::from_chars(osiSymbol_.data() + osiSymbolLen - strikePriceStrLen, osiSymbol_.data() + osiSymbolLen, strikePrice).ec != std::errc{})
       {
         std::cout << "Error: Failed to get strike price for osi symbol - " << osiSymbol_ << std::endl;
+        return;
       }
-      else
-      {
-        strikePrice = strikePrice / 1000;
-      }
+
+      strikePrice = strikePrice / scaleFactor;
     }
 
-    void printSymbolInfo() const
-    {
-      std::cout << "ROOT Symbol: " << rootSymbol << std::endl
-                << "Expiration : " << expirationDate << std::endl
-                << "CallOrPut  : " << callOrPut << std::endl
-                << "Strike     : " << strikePrice << std::endl;
-    }
-  private:
-    size_t pos{0};
+    uint8_t osiSymbolLen{0};
+    static constexpr uint8_t strikePriceStrLen{8};
+    static constexpr uint8_t maxSymbolLength{6}; // 6 characters symbol (no space padding)
+    static constexpr uint8_t expirationDateLength{6};
+    static constexpr uint16_t scaleFactor{1000};
 };
 
 class osiSymbolParser
@@ -94,6 +93,7 @@ class osiSymbolParser
     {
       parseOsiSymbol(symbol_);
     }
+
     ~osiSymbolParser() {}
 
     void display() const
@@ -102,21 +102,32 @@ class osiSymbolParser
     }
 
   private:
-    int parseOsiSymbol(const std::string& symbol_)
+    bool isValidOsiSymbol(const std::string& symbol_)
     {
-      if (symbol_.length() > OSI_SYMBOL_LENGTH ||
-          symbol_.length() < (OSI_SYMBOL_LENGTH - 5) /* Minimum length of one character OSI symbol  */)
+      // Fow now, only validate length, we can add more checks if needed.
+      if (symbol_.length() > osiSymbolMaxLength ||
+          symbol_.length() < (osiSymbolMinLength - 5))
       {
         std::cout << "Error: Invalid OSI symbol - " << symbol_ << std::endl;
+        return false;
       }
 
-      m_symbol.getOsiSymbolInfo(symbol_);
+      return true;
+    }
 
-      std::cout << symbol_ << std::endl;
-      return 0;
+    void parseOsiSymbol(const std::string& symbol_)
+    {
+      if (isValidOsiSymbol(symbol_))
+      {
+        std::cout << "OSI Symbol : " << symbol_ << std::endl;
+        m_symbol.getOsiSymbolInfo(symbol_);
+      } 
     }
 
     mutable osiSymbol m_symbol;
+
+    static constexpr uint8_t osiSymbolMaxLength{21};
+    static constexpr uint8_t osiSymbolMinLength{osiSymbolMaxLength - 5}; // Minimum length of one character OSI symbol
 };
 
 int main()
@@ -125,7 +136,7 @@ int main()
   osiSymbolParser tsla("TSLA 250216P00500000");
   tsla.display();
 
-  osiSymbolParser test("TEST.A260216P00000150");
+  osiSymbolParser test("TEST.A260216C00000150");
   test.display();
 
   return 0;
